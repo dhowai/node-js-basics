477397a7b00929f21b4144c53af9aed809bc9a0a
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2023-01-25 15:07:48 +0000

    Use 3rd party modules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const slugify = require("slugify");
 const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
 
@@ -20,6 +21,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
